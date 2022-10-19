5c58631a0891c1a38b8b6d95a1d1c68909fd58ce
BobbyDeluna <bobby.deluna2@gmail.com>
2022-10-18 23:41:10 -0700

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,15 @@ function App() {
 
 const [dogs, setDogs] = useState([]);
 const [doggo, setDoggo] = useState([]);
+const [filter, setFilter] = useState(true);
+const [holdDogs, setHoldDogs] = useState([]);
+const [render, setRender] = useState(true);
 
   useEffect(()=>{
     fetch(`http://localhost:3001/pups/`)
     .then(res=>res.json())
     .then(data=>{console.log(data);setDogs(data)})
-  },[]);
+  },[render]);
 
   const displayDogs = dogs.map((dog)=>{
     return (
@@ -20,12 +23,44 @@ const [doggo, setDoggo] = useState([]);
   function handleClick(dog){
     fetch(`http://localhost:3001/pups/${dog.id}`)
     .then(res=>res.json())
-    .then(data=>{console.log(data); setDoggo(data)})}    
+    .then(data=>{console.log(data); setDoggo(data)})} 
+    
+    function handleGoodDog(){
+
+      fetch(`http://localhost:3001/pups/${doggo.id}`,
+      {method:'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body:JSON.stringify({
+        id: doggo.id,
+        name: doggo.name,
+        isGoodDog: !doggo.isGoodDog,
+        image: doggo.image
+      })
+    })
+    .then(res=>res.json())
+    .then(data => setDoggo(data));
+
+    if(filter === true && doggo.isGoodDog === false){
+      setRender(!render)
+    }
+  }
+    function handleFilter(){
+      setFilter(!filter);
+      setHoldDogs(dogs);
+      if(filter === true ){
+        setDogs(dogs.filter(dog=>dog.isGoodDog === true))
+      }
+      else if(filter === false){
+        setDogs(holdDogs);
+      }
+    }
 
   return (
     <div className="App">
       <div id="filter-div">
-        <button id="good-dog-filter">Filter good dogs: OFF</button>
+        <button id="good-dog-filter" onClick={handleFilter}>Filter good dogs: {filter ? "OFF" : "ON"}</button>
       </div>
       <div id="dog-bar">{displayDogs}</div>
       <div id="dog-summary-container">
@@ -33,7 +68,7 @@ const [doggo, setDoggo] = useState([]);
         <div id="dog-info">
             <img src={doggo.image} alt={doggo.name} />
         <h2>{doggo.name}</h2>
-        <button>Good Dog!</button>
+        <button onClick={handleGoodDog}>{doggo.isGoodDog ? "Good Dog!" : "Bad Dog!" }</button>
 </div>
       </div>
     </div>
